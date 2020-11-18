1b6956019b5a6bf2f1b4638388f72247c7d1adb9
ningjngjing0614 <215937309@qq.com>
2020-11-18 22:35:04 +0800

    宁静静：实验

---
+++ 11.18.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+//#include<string.h>
+int main()
+{
+	//char a = '10';
+	//short b = 10;
+	//整型类型
+	//printf ("sizeof(char) is %d\n",sizeof(char));
+	//printf ("sizeof(short) is %d\n",sizeof(short));
+	//printf ("sizeof(int) is %d\n",sizeof(int));
+	//printf ("sizeof(long) is %d\n",sizeof(long));
+	//printf ("sizeof(long long) is %d\n",sizeof(long long));
+	
+	////printf ("sizeof(bool) is %d\n",sizeof(bool));
+	//浮点类型
+	//printf ("sizeof(float) is %d\n",sizeof(float));
+	//printf ("sizeof(double) is %d\n",sizeof(double));
+	////printf ("sizeof(short) is %d\n",sizeof(long double));
+	//
+	//printf ("%c\n",a);
+	//printf ("%hd\n",b);
+	//char ch[] = "bit";
+	//char ch3[] = "bb";
+	//char ch1[] ={'bit'};
+	//char ch2[12] = {'b','t','i'};
+	//int a = 20;
+	/*printf("%d\n",sizeof(ch));
+	printf("%d\n",strlen(ch1));
+	printf("%d\n",sizeof(ch2));
+	printf("%d\n",strlen(ch2));
+	printf("%d\n",strlen(ch3));*/
+	/*printf("%x",a);
+	printf("%o\n",a);
+	printf("%d\n",strlen("c:\code\test.c\n"));
+	printf("%d\n",strlen("c:\test\123"));
+	printf("%d\n",strlen("\123"));
+	printf("%d\n",strlen("\n"));
+	printf("%d\n",strlen("\t"));
+	printf("c:\code\test.c\n");*/
+	/*printf("     **     \n");
+	printf("     **     \n");
+    printf("************\n");
+	printf("************\n");
+    printf("    *  *    \n");
+	printf("    *  *    \n");*/
+	//printf("%lf\n",456.23232322222222223);
+	printf("hello");
+return 0;
+}
