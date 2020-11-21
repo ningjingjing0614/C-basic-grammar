44fbb1fdc89b7fe8d7c77cae6283083f91e29250
ningjngjing0614 <215937309@qq.com>
2020-11-21 21:31:19 +0800

    宁静静：试验

---
+++ 11.21.c
@@ -0,0 +1,50 @@
+//#include<stdio.h>
+//int main()
+//{
+//	printf("int 型在本机的字节数为：%d", sizeof (int));
+	//int num = 12;
+	//num = 34;
+	//num = 345;
+	//printf("%d",num);
+	////char ch1 = 'weieee';//char 单引号内只能定义一个字符，常量中字符太多会出现错误语法错误
+	//char ch2 = 'c';
+	//char ch3[] = "C语言";
+	//printf ("ch2--%c, ch3--%s\n", ch2, ch3);
+	//int num1 = 564;
+	//long num2 = 564;
+	//char num3 = 564;
+	//printf ("num1 = %.5d num2 = %.5d num3 = %.5d",num1, num2, num3);
+	/*double a = 2.333333333;
+	printf("%.9lf\n",a);*/
+	/*float num1 = 3.1415926;
+	float num2 = 3.1415926L;
+	double num3 = 3.1415926;
+	printf ("num1 = %f num2 = %f num3 = %.8f\n",num1, num2, num3);*/
+	//float num1 = 3.14154444444L;
+	//double num2 = 3.141595555;
+	//float num3 = 3.1415L;
+	//double num4 = 3.1415;
+	//float num5 = 3.14154444444;
+	//printf("num1 = %0.8f num2 = %.8f num3 = %.8f num4 = %.8f num5 = %.8f",num1, num2, num3, num4, num5);
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+	float num1 = 3.14154444444L;
+	double num2 = 3.141595555;
+	float num3 = 3.1415L;
+	double num4 = 3.1415;
+	float num5 = 3.14154444444;
+	printf("num1 = %0.8f num2 = %.8f num3 = %.8f num4 = %.8f num5 = %.8f",num1, num2, num3, num4, num5);
+	return 0;
+}
+
+/*printf ("hkkkello\rword");
+	printf("张无忌诛之若\r擦擦擦擦");*/
+	/*float a = 3.1415926;
+	long b = 56l;
+	printf("%d\n",a);
+	printf("%ld\n",b);
+	
+	*/
