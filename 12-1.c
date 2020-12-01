8023456a1221257068c7d036db325ca6c552ee3f
ningjngjing0614 <215937309@qq.com>
2020-12-01 14:03:21 +0800

    宁静静：test

---
+++ 12-1.c
@@ -0,0 +1,35 @@
+	//int str[12];
+	//scanf ("%d",str);
+	//printf("%d",str);
+	//int a = 1;
+	//while (a < 11)
+	//{
+	//	printf("%d",a);
+	//	a += 2;
+	//}
+	//
+	/*int num, ret = 1;
+	int a = 0;
+	for (num = 1; num < 3; num++)
+	{
+		ret *= num;
+		a += ret;
+		
+	}
+	printf("%d",ret);
+	printf("%d",a);*/
+	/*int num = 1;
+	int ret = 1;
+	while (num < 10)
+	{
+		ret *= num;
+		num ++;
+	}
+	printf("%d\n",ret);*/
+
+#include <stdio.h>
+int main()//¼ÆËãnµÄ½×³Ë
+{
+
+	return 0;
+}
