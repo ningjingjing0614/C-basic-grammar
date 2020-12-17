327bc3db56d027bf49e67761523795e3d3caf7b5
ningjngjing0614 <215937309@qq.com>
2020-12-17 18:49:00 +0800

    宁静静：test

---
+++ 12-17.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[] = "1,2,3 ";
+	printf("%d  %d\n",sizeof(str), strlen(str));
+	//printf("%deee\n",printf("%d",11118));
+	return 0;
+}
+
