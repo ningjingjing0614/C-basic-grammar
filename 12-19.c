5dc562414a1781f0252179931107953cb63e45f5
ningjngjing0614 <215937309@qq.com>
2020-12-19 22:26:08 +0800

    宁静静：test

---
+++ 12-19.c
@@ -0,0 +1,37 @@
+//#include <stdio.h>
+//void add()
+//{
+//static int num = 0;//局部变量定义时前面加上static关键字
+//	int num = 0;
+//	num ++;
+//	printf("%d\n",num);
+//}
+//int main()
+//{
+//	int i;
+//	for(i = 0; i < 5; i ++)
+//	{
+//	    add();
+//	}
+//	return 0;
+//}
+
+/*#include <stdio.h>
+//extern int i;
+////static int i = 9;
+//extern void as();
+//int main()
+//{
+//	 as();
+//	printf("%d\n",i);
+//	return 0;
+//}
+//
+////#include <stdio.h>
+////
+////void add()
+////{
+////static int i = 0;
+////i ++;
+////printf("%d\n",i);
+////}
